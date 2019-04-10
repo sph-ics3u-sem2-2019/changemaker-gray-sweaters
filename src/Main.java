305e853a5792ef4ab53b6b49de04e9192e4b2b19
chris2413 <47532316+chris2413@users.noreply.github.com>
2019-04-10 10:52:09 -0400

    Projects

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,49 @@ public class Main {
 		System.out.println("How much was paid?");
 		double payment=in.nextDouble();
 		//TODO: Here is where your code goes
+		double change=(payment-cost);
+		int num100s;
+		int num50s;
+		int num20s;
+		int num10s;
+		int num5s;
+		int numtwoonies;
+		int numloonies;
+		int numquarters;
+		int numdimes;
+		int numnickels;
+		
+		
+		
+		System.out.println("your change is "+change);
+		num100s=(int)(change/100);
+		change=change%100;
+		System.out.println(num100s+" x $100");
+		num50s=(int)(change/50);
+		change=change%50;
+		System.out.println(num50s+" x $50");
+		num20s=(int)(change/20);
+		change=change%20;
+		System.out.println(num20s+"x $20");
+		num10s=(int)(change/10);
+		System.out.println(num10s+" x $10");
+		num5s=(int)(change/5);
+		System.out.println(num5s+"x $5");
+		numtwoonies=(int)(change/2);
+		System.out.println(numtwoonies+" x $2");
+		numloonies=(int)(change/1);
+		System.out.println(numloonies+"x $1");
+		numquarters=(int)(change/0.25);
+		System.out.println(numquarters+"x $0.25");
+		numdimes=(int)(change/0.10);
+		System.out.println(numdimes+" x $0.10");
+		numnickels=(int)(change/0.05);
+		System.out.println(numnickels+"x $0.05");
+		
+		
+		
+				
+
 	}
 
 }
